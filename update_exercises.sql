8ac93e65c1a1f4c390be8b2e34dc1b0b5bc62b2f
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-05-26 15:12:07 -0500

    Feat: updated albums.

--- update_exercises.sql
+++ update_exercises.sql
@@ -2,9 +2,17 @@ USE codeup_test_db;
 
 -- All albums in your table
 SELECT * FROM albums;
+UPDATE albums
+SET sales = sales * 10;
 
 -- all albums released before 1980
 SELECT * FROM albums WHERE release_date < 1980;
+UPDATE albums
+SET release_date = 1800
+WHERE release_date < 1980;
 
 -- All albums by Michael Jackson
 SELECT * FROM albums WHERE artist = 'Michael Jackson';
+UPDATE albums
+SET artist = 'Peter Jackson'
+WHERE artist = 'Michael Jackson';
