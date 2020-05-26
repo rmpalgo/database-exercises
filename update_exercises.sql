df84dc8431aa8395cc8bd43a2d102b3b27aaadf2
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-05-26 15:17:54 -0500

    Feat: added select to show but unable due to recompiling of script causig out of range error.

--- update_exercises.sql
+++ update_exercises.sql
@@ -4,15 +4,18 @@ USE codeup_test_db;
 SELECT * FROM albums;
 UPDATE albums
 SET sales = sales * 10;
+SELECT sales FROM albums;
 
 -- all albums released before 1980
 SELECT * FROM albums WHERE release_date < 1980;
 UPDATE albums
 SET release_date = 1800
 WHERE release_date < 1980;
+SELECT release_date FROM albums WHERE release_date = 1800;
 
 -- All albums by Michael Jackson
 SELECT * FROM albums WHERE artist = 'Michael Jackson';
 UPDATE albums
 SET artist = 'Peter Jackson'
 WHERE artist = 'Michael Jackson';
+SELECT artist FROM albums WHERE artist = 'Peter Jackson';
