c6b831f8b75a4189fa21d4327c7aa9418bcd9f80
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-05-26 15:44:28 -0500

    Feat: changed to DELETE keyword and executed script.

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -1,10 +1,10 @@
 USE codeup_test_db;
 
 SELECT 'Albums released after 1991' AS 'Info';
-SELECT * FROM albums WHERE release_date > 1991;
+DELETE FROM albums WHERE release_date > 1991;
 
 SELECT 'Albums with genre Disco' AS 'Info';
-SELECT * FROM albums WHERE genre = 'Disco';
+DELETE FROM albums WHERE genre = 'Disco';
 
 SELECT 'Albums by Whitney Houston' AS 'Info';
-SELECT * FROM albums WHERE artist = 'Whitney Houston';
+DELETE FROM albums WHERE artist = 'Whitney Houston';
