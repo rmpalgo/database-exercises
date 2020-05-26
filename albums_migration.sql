2e6e382be2252943f6d92c6ad1f57fd8848ff415
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-05-26 10:28:30 -0500

    Completed exercise.

--- albums_migration.sql
+++ albums_migration.sql
@@ -2,10 +2,10 @@ USE codeup_test_db;
 DROP TABLE IF EXISTS albums;
 CREATE TABLE albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    artist VARCHAR(50) DEFAULT 'NONE',
+    artist VARCHAR(50) NOT NULL,
     name VARCHAR(50) NOT NULL,
-    release_date INT,
-    sales DOUBLE,
+    release_date INT NOT NULL,
+    sales DECIMAL(6, 3) NOT NULL,
     genre VARCHAR(50),
     PRIMARY KEY(id)
 );
