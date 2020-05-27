eaf620209861ae34d97873f50763499ff10c77e2
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-05-27 14:56:47 -0500

    Feat: completed exercise, pre-walkthrough.

--- update_exercises.sql
+++ update_exercises.sql
@@ -8,11 +8,14 @@ SELECT sales FROM albums;
 
 SELECT 'All albums released before 1980' AS 'Info';
 SELECT * FROM albums WHERE release_date < 1980;
+
 UPDATE albums
-SET release_date = 1800
+SET release_date = release_date - 1800
 WHERE release_date < 1980;
+
 SELECT 'All albums changed to 1800' AS 'Info';
-SELECT release_date FROM albums WHERE release_date = 1800;
+SELECT release_date FROM albums
+WHERE release_date < 1800;
 
 -- All albums by Michael Jackson
 SELECT 'All albums by Michael Jackson' AS 'Info';
@@ -20,5 +23,6 @@ SELECT * FROM albums WHERE artist = 'Michael Jackson';
 UPDATE albums
 SET artist = 'Peter Jackson'
 WHERE artist = 'Michael Jackson';
+
 SELECT 'All albums by Michael Jackson changed name to Peter Jackson' AS 'Info';
 SELECT artist FROM albums WHERE artist = 'Peter Jackson';
