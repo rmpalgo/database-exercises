7db70dc862eae1e2474493a7ab3cd3278447037f
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-05-28 12:04:45 -0500

    Feat: completed first bonus exercises.

--- bonus_exercises.sql
+++ bonus_exercises.sql
@@ -4,8 +4,23 @@ SHOW tables;
 
 DESCRIBE titles;
 
-SELECT * FROM titles
-    ORDER BY title;
+SELECT DISTINCT title FROM titles;
 
 SELECT * FROM employees
 LIMIT 10;
+
+SELECT first_name
+FROM employees
+GROUP BY first_name;
+
+SELECT DISTINCT first_name
+FROM employees;
+
+SELECT * FROM titles
+WHERE title = 'Engineer';
+
+SELECT title, COUNT(title) AS 'count'
+FROM titles GROUP BY title
+ORDER BY count DESC
+LIMIT 1;
+
