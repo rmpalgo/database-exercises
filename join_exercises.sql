26ae32f9bfba518a9c6cfbb7cb6d5d9e1811bbad
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-05-29 14:52:55 -0500

    Feat: completed second exercise for subqueries.

--- join_exercises.sql
+++ join_exercises.sql
@@ -7,19 +7,19 @@ SELECT ds.dept_name AS 'Department Name',
 CONCAT(em.first_name, ' ', em.last_name) AS 'Department Manager'
 FROM employees AS em
 JOIN dept_manager AS dm
-    ON dm.emp_no = em.emp_no
+ON dm.emp_no = em.emp_no
 JOIN departments AS ds
-    ON dm.dept_no = ds.dept_no
-WHERE dm.to_date >= NOW()
+ON dm.dept_no = ds.dept_no
+WHERE dm.to_date >= CURDATE()
 ORDER BY ds.dept_name;
 
 SELECT ds.dept_name AS 'Department Name',
-       CONCAT(em.first_name, ' ', em.last_name) AS 'Department Manager'
+CONCAT(em.first_name, ' ', em.last_name) AS 'Department Manager'
 FROM employees AS em
-    JOIN dept_manager AS dm
-    ON dm.emp_no = em.emp_no
-    JOIN departments AS ds
-    ON dm.dept_no = ds.dept_no
+JOIN dept_manager AS dm
+ON dm.emp_no = em.emp_no
+JOIN departments AS ds
+ON dm.dept_no = ds.dept_no
 WHERE dm.to_date >= NOW()
 AND em.gender = 'F'
 ORDER BY ds.dept_name;
@@ -39,7 +39,7 @@ GROUP BY title;
 
 SELECT ds.dept_name AS 'Department Name',
 CONCAT(em.first_name, ' ', em.last_name) AS 'Department Manager',
-       s.salary AS Salary
+s.salary AS Salary
 FROM employees AS em
 JOIN dept_manager AS dm
 ON dm.emp_no = em.emp_no
@@ -49,4 +49,24 @@ JOIN salaries s on em.emp_no = s.emp_no
 WHERE dm.to_date >= NOW()
 AND s.to_date >= NOW()
 ORDER BY ds.dept_name;
+
+
+SELECT * FROM employees LIMIT 10;
+-- Bonus
+SELECT CONCAT(em.first_name, ' ', em.last_name) AS 'Employee Name',
+d.dept_name AS 'Department Name'
+FROM dept_emp AS de
+JOIN employees AS em
+    ON de.emp_no = em.emp_no
+JOIN departments d
+    ON de.dept_no = d.dept_no
+JOIN dept_manager dm on d.dept_no = dm.dept_no
+JOIN dept_emp AS demp ON demp.dept_no = d.dept_no
+WHERE de.to_date > CURDATE()
+ORDER BY em.first_name;
+
+
+
+
+
 
--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -5,3 +5,18 @@ WHERE hire_date IN (
     SELECT hire_date from employees
     WHERE emp_no = 101010
           );
+
+SELECT titles.title FROM titles
+WHERE emp_no IN (
+    SELECT emp_no FROM employees
+    WHERE first_name = 'Aamod'
+    );
+
+SELECT DISTINCT titles.title FROM titles
+WHERE emp_no IN (
+    SELECT emp_no FROM employees
+    WHERE first_name = 'Aamod'
+);
+
+
+
