446477ab7ff0e4c13915ebe72d6f282dc95675ef
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-05-30 21:06:40 -0500

    Feat: completed bonus for joins exercises.

--- join_exercises.sql
+++ join_exercises.sql
@@ -52,18 +52,19 @@ ORDER BY ds.dept_name;
 
 
 SELECT * FROM employees LIMIT 10;
+
 -- Bonus
-SELECT CONCAT(em.first_name, ' ', em.last_name) AS 'Employee Name',
-d.dept_name AS 'Department Name'
-FROM dept_emp AS de
-JOIN employees AS em
-    ON de.emp_no = em.emp_no
-JOIN departments d
-    ON de.dept_no = d.dept_no
-JOIN dept_manager dm on d.dept_no = dm.dept_no
-JOIN dept_emp AS demp ON demp.dept_no = d.dept_no
-WHERE de.to_date > CURDATE()
-ORDER BY em.first_name;
+SELECT CONCAT(e.first_name, ' ', e.last_name),
+dept_name AS 'Department Name',
+CONCAT(emng.first_name, ' ', emng.last_name) AS 'Department Manager'
+FROM employees AS e
+JOIN dept_emp AS d ON d.emp_no = e.emp_no
+JOIN departments AS dp ON dp.dept_no = d.dept_no
+JOIN dept_manager  AS dm ON dm.dept_no = dp.dept_no
+JOIN employees AS emng ON emng.emp_no = dm.emp_no
+WHERE dm.to_date > CURDATE() AND d.to_date > CURDATE()
+ORDER BY dept_name;
+
 
 
 
