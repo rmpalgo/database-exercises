f8be03e10ef1890642d4394292144745d49fc13f
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-05-31 19:43:26 -0500

    Feat: fixed indeces unique adding constraints.

--- albums_migration.sql
+++ albums_migration.sql
@@ -2,10 +2,11 @@ USE codeup_test_db;
 DROP TABLE IF EXISTS albums;
 CREATE TABLE albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    artist VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255) NOT NULL UNIQUE,
+    artist VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL,
     release_date INT NOT NULL,
     sales DECIMAL(6, 3) NOT NULL,
     genre VARCHAR(50),
-    PRIMARY KEY(id)
+    PRIMARY KEY(id),
+    CONSTRAINT combo UNIQUE (artist, name)
 );
